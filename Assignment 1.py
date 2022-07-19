c5b6f2de8f1bdee8f4d42e6b676d04227c7e13c0
David Ampofo Quarcoo <106585810+Beloved21@users.noreply.github.com>
2022-07-19 22:05:35 +0000

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,6 +1,6 @@
 while True:
-    n = int(input("Enter number of row: "))    
-    for i in range(1, n+1):
+    n = int(input("Enter number of row: "))    # This tells the number of rows to print
+    for i in range(1, n+1):                    # It iterates the given conditions and executes trhe following each if statements when the conditions are satisfied
         if i ==1:
             print('  *')
         if i==2:    
